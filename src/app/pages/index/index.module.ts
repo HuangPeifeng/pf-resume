8c0bae480bfecbdf3e3a114ee3e49d8f58c5f0d0
peifeng1115 <peifeng1115@gmil.com>
2019-09-18 22:10:43 +0800

    header component

--- src/app/pages/index/index.module.ts
+++ src/app/pages/index/index.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { IndexRoutingModule } from './index-routing.module';
 import { IndexComponent } from './index.component';
+import { ThemeModule } from 'src/app/theme/theme.module';
 
 
 @NgModule({
   declarations: [IndexComponent],
   imports: [
     CommonModule,
-    IndexRoutingModule
+    IndexRoutingModule,
+    ThemeModule
   ]
 })
 export class IndexModule { }
---
+++ src/app/theme/theme.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components';
+
+const COMPONENTS = [
+  HeaderComponent
+];
+
+@NgModule({
+  declarations: [
+    COMPONENTS
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    COMPONENTS
+  ]
+})
+export class ThemeModule { }
