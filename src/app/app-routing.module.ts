5ebf580cb026391b3ce1462a1deddcd7570327c9
peifeng1115 <peifeng1115@gmil.com>
2019-09-18 22:00:46 +0800

    首頁 初步刻版

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'index' },
+  { path: 'index', loadChildren: './pages/index/index.module#IndexModule' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
