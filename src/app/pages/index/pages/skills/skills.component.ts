cf7901eb847afd17abb4093ebc25524c41a92658
peifeng1115 <peifeng1115@gmil.com>
2019-09-19 21:00:22 +0800

    skills 初步刻版

--- src/app/pages/index/pages/skills/skills.component.ts
+++ src/app/pages/index/pages/skills/skills.component.ts
@@ -3,9 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
-  styleUrls: ['./skills.component.css']
+  styleUrls: ['./skills.component.scss']
 })
 export class SkillsComponent implements OnInit {
+
+  skills = [
+    { name: 'Angular 2+', percent: 90 },
+    { name: 'Typescript', percent: 80 },
+    { name: 'Javascript', percent: 80 },
+    { name: 'Html', percent: 80 },
+    { name: 'CSS & SCSS', percent: 80 },
+    { name: 'JQuery', percent: 70 },
+    { name: 'Git', percent: 70 },
+    { name: 'Express', percent: 50 },
+    { name: 'MySQL', percent: 50 },
+  ];
 
   constructor() { }
 
