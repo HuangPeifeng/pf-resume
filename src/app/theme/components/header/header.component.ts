d0429fbecbdb97395f986732eafd3e2d33972a9e
peifeng1115 <peifeng1115@gmail.com>
2019-09-20 11:53:12 +0800

    修改header版面

--- src/app/theme/components/header/header.component.ts
+++ src/app/theme/components/header/header.component.ts
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  menuList = [
+    { name: '關於我' },
+    { name: '技能' },
+    { name: '學經歷' },
+    { name: '專案與作品' }
+  ];
+
+  menuOpen = false;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  toggle() {
+    this.menuOpen = !this.menuOpen;
   }
 
 }
