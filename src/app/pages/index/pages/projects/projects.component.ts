cfaf1ca09989939346e908a6955b7a3cfd5de25d
peifeng1115 <peifeng1115@gmail.com>
2019-09-23 13:13:26 +0800

    projects 完成

--- src/app/pages/index/pages/projects/projects.component.ts
+++ src/app/pages/index/pages/projects/projects.component.ts
@@ -9,8 +9,65 @@ import { ActivatedRoute } from '@angular/router';
 export class ProjectsComponent implements OnInit, AfterViewInit {
   @ViewChild('main', { static: false }) main: ElementRef<any>;
 
-  constructor(
+  projectList = [
+    {
+      name: 'Magic Modules',
+      coverUrl: 'assets/images/magic_modules_logo.png',
+      url: 'http://magic-modules.hamastar.com.tw'
+    },
+    {
+      name: '富邦電子書城',
+      coverUrl: 'assets/images/fubon_book_logo.png',
+      url: 'https://fubonebook.hamastar.com.tw'
+    },
+    {
+      name: '富邦新視界',
+      coverUrl: 'assets/images/fubon_video_logo.png',
+      url: 'https://video.fubonlife.com.tw'
+    },
+    {
+      name: '跨閱互動教育平台',
+      coverUrl: 'assets/images/kai_logo.png',
+      url: 'https://ebook.kaipublish.com.hk'
+    },
+    {
+      name: '臺北市政府電子公報資訊網',
+      coverUrl: 'assets/images/tpgz_logo.png',
+      url: 'https://gazette.gov.taipei'
+    },
+    {
+      name: 'BSL俱樂部',
+      coverUrl: 'assets/images/bsl_logo.png',
+      url: 'https://lcy-stage.project.lightup-studio.com'
+    },
+    {
+      name: '理特尚聖誕節促銷網頁',
+      coverUrl: 'assets/images/littlesun_logo.png',
+      url: 'https://huangpeifeng.github.io/Littlesun/'
+    },
+    {
+      name: '潛行者事務所',
+      coverUrl: 'assets/images/divewalker_logo.jpg',
+      url: 'https://divewalker.project.lightup-studio.com'
+    },
+    {
+      name: '跑馬燈APP',
+      coverUrl: 'assets/images/marquee_logo.png',
+      url: 'https://marquee.project.lightup-studio.com'
+    },
+    {
+      name: 'AdalaArt',
+      coverUrl: 'assets/images/adalaart_logo.png',
+      url: 'https://adalaart.com'
+    },
+    {
+      name: 'npm',
+      coverUrl: 'assets/images/npm_logo.png',
+      url: 'https://www.npmjs.com/settings/peifeng1115/packages'
+    }
+  ];
 
+  constructor(
     private route: ActivatedRoute
   ) { }
 
@@ -23,6 +80,10 @@ export class ProjectsComponent implements OnInit, AfterViewInit {
         this.main.nativeElement.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'start' });
       }
     });
+  }
+
+  openLink(item) {
+    window.open(item.url, '_blank');
   }
 
 }
