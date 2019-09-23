47232b5c3aeb5ba1ff8c1f988801f26c541ac3d2
peifeng1115 <peifeng1115@gmail.com>
2019-09-23 14:33:04 +0800

    go top

--- src/app/pages/index/pages/home/home.component.ts
+++ src/app/pages/index/pages/home/home.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
+  @ViewChild('main', { static: false }) main: ElementRef<any>;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    this.route.fragment.subscribe(res => {
+      if (res === 'home') {
+        this.main.nativeElement.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'start' });
+        this.router.navigate([], {
+          fragment: null
+        });
+      }
+    });
   }
 
 }
