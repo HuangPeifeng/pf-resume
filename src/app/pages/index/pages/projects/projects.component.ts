134ad2b4f9482468f99fb137d80956d64b5b8da1
黃培峰 <peifeng1115@gmail.com>
2020-12-02 10:12:19 +0800

    feat: fixed projects.

--- src/app/pages/index/pages/projects/projects.component.ts
+++ src/app/pages/index/pages/projects/projects.component.ts
@@ -1,88 +1,105 @@
-import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import {
+  Component,
+  OnInit,
+  AfterViewInit,
+  ViewChild,
+  ElementRef
+} from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-projects',
-  templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.scss']
+  selector: "app-projects",
+  templateUrl: "./projects.component.html",
+  styleUrls: ["./projects.component.scss"]
 })
 export class ProjectsComponent implements OnInit, AfterViewInit {
-  @ViewChild('main', { static: false }) main: ElementRef<any>;
+  @ViewChild("main", { static: false }) main: ElementRef<any>;
 
   projectList = [
     {
-      name: 'Magic Modules',
-      coverUrl: 'assets/images/magic_modules_logo.png',
-      url: 'http://magic-modules.hamastar.com.tw'
+      name: "Magic Modules",
+      coverUrl: "assets/images/magic_modules_logo.png",
+      url: "http://magic-modules.hamastar.com.tw"
     },
     {
-      name: '富邦電子書城',
-      coverUrl: 'assets/images/fubon_book_logo.png',
-      url: 'https://fubonebook.hamastar.com.tw',
-      isPreview: 'assets/images/fubon_ebook.png'
+      name: "富邦電子書城",
+      coverUrl: "assets/images/fubon_book_logo.png",
+      url: "https://fubonebook.hamastar.com.tw",
+      isPreview: "assets/images/fubon_ebook.png"
     },
     {
-      name: '富邦新視界',
-      coverUrl: 'assets/images/fubon_video_logo.png',
-      url: 'https://video.fubonlife.com.tw',
-      isPreview: 'assets/images/fubon_video.png'
+      name: "富邦新視界",
+      coverUrl: "assets/images/fubon_video_logo.png",
+      url: "https://video.fubonlife.com.tw",
+      isPreview: "assets/images/fubon_video.png"
     },
     {
-      name: '跨閱互動教育平台',
-      coverUrl: 'assets/images/kai_logo.png',
-      url: 'https://ebook.kaipublish.com.hk'
+      name: "跨閱互動教育平台",
+      coverUrl: "assets/images/kai_logo.png",
+      url: "https://ebook.kaipublish.com.hk"
     },
     {
-      name: '臺北市政府電子公報資訊網',
-      coverUrl: 'assets/images/tpgz_logo.png',
-      url: 'https://gazette.gov.taipei'
+      name: "臺北市政府電子公報資訊網",
+      coverUrl: "assets/images/tpgz_logo.png",
+      url: "https://gazette.gov.taipei"
     },
     {
-      name: 'BSL俱樂部',
-      coverUrl: 'assets/images/bsl_logo.png',
-      url: 'https://lcy-stage.project.lightup-studio.com'
+      name: "BSL俱樂部",
+      coverUrl: "assets/images/bsl_logo.png",
+      url: "https://lcy-stage.project.lightup-studio.com"
     },
     {
-      name: '理特尚聖誕節促銷網頁',
-      coverUrl: 'assets/images/littlesun_logo.png',
-      url: 'https://huangpeifeng.github.io/Littlesun/'
+      name: "理特尚聖誕節促銷網頁",
+      coverUrl: "assets/images/littlesun_logo.png",
+      url: "https://huangpeifeng.github.io/Littlesun/"
     },
     {
-      name: '潛行者事務所',
-      coverUrl: 'assets/images/divewalker_logo.jpg',
-      url: 'https://divewalker.project.lightup-studio.com'
+      name: "潛行者事務所",
+      coverUrl: "assets/images/divewalker_logo.jpg",
+      url: "https://divewalker.project.lightup-studio.com"
     },
     {
-      name: '跑馬燈APP',
-      coverUrl: 'assets/images/marquee_logo.png',
-      url: 'https://marquee.project.lightup-studio.com'
+      name: "良田",
+      coverUrl: "assets/images/life-it.svg",
+      url: "https://life-lt.com"
     },
     {
-      name: 'AdalaArt',
-      coverUrl: 'assets/images/adalaart_logo.png',
-      url: 'https://adalaart.com'
+      name: "品工治所",
+      coverUrl: "assets/images/surroundings.svg",
+      url: "https://surroundings.project.lightup-studio.com/"
     },
     {
-      name: 'npm',
-      coverUrl: 'assets/images/npm_logo.png',
-      url: 'https://www.npmjs.com/~peifeng1115'
+      name: "江阴康瑞",
+      coverUrl: "assets/images/krs.svg",
+      url: "https://krs.project.lightup-studio.com/"
+    },
+    {
+      name: "創科智盈",
+      coverUrl: "assets/images/int.svg",
+      url: "https://int.lightup-studio.com"
+    },
+    {
+      name: "npm",
+      coverUrl: "assets/images/npm_logo.png",
+      url: "https://www.npmjs.com/~peifeng1115"
     }
   ];
 
   showPreview = false;
   previewUrl = null;
 
-  constructor(
-    private route: ActivatedRoute
-  ) { }
+  constructor(private route: ActivatedRoute) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   ngAfterViewInit() {
     this.route.fragment.subscribe(res => {
-      if (res === 'projects') {
-        this.main.nativeElement.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'start' });
+      if (res === "projects") {
+        this.main.nativeElement.scrollIntoView({
+          behavior: "smooth",
+          block: "start",
+          inline: "start"
+        });
       }
     });
   }
@@ -92,7 +109,7 @@ export class ProjectsComponent implements OnInit, AfterViewInit {
       this.previewUrl = item.isPreview;
       this.showPreview = true;
     } else {
-      window.open(item.url, '_blank');
+      window.open(item.url, "_blank");
     }
   }
 
@@ -102,5 +119,4 @@ export class ProjectsComponent implements OnInit, AfterViewInit {
       this.previewUrl = null;
     }
   }
-
 }
